4df66b46a0bd0443812fb0b85b1393d8c9b42365
nithishk2024aids-cell <nithish.k2024aids@sece.ac.in>
2026-05-05 08:18:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,14 +4,22 @@ import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // important for cloud DBs
+        },
+    },
 });
 
 const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('PostgreSQL Connected');
+
         await sequelize.sync({ alter: true });
         console.log('Database synced');
+
     } catch (error) {
         console.error('DB Connection Error:', error.message || error);
         console.error('Original:', error.original?.message);
